272038988887456c077b67f3ba4b0b688ea5aee3
avishsa <avishag1991@gmail.com>
2021-08-05 11:17:55 +0300

    style header

--- src/Components/boilerplate/Header.js
+++ src/Components/boilerplate/Header.js
@@ -4,9 +4,13 @@ import '../../css/boilerplate/Header.css';
 
 class Header extends Component {
     render() {
-        return (<header   className="d-flex flex-column-reverse justify-content-between">
-            <img src="./images/logo-app.png" className="d-flex flex-row"  />
-            <NavApp/>
+        return (<header className=" container w-75-xl d-flex m-5-xl flex-row justify-content-center">
+            <div className="w-100 d-flex flex-column-reverse">
+                <div className="d-flex flex-row justify-content-start">
+                    <img src="./images/logo-app.png" />
+                </div>
+                <NavApp />
+            </div>
         </header>)
     }
 }
--- src/Components/boilerplate/NavApp.js
+++ src/Components/boilerplate/NavApp.js
@@ -1,7 +1,7 @@
 import React, { Component }  from 'react';
 class NavApp extends Component {
     render() {
-        return (<nav className=" d-flex flex-row-reverse justify-content-start navbar  navbar-light ">
+        return (<nav className=" d-flex flex-row text-end justify-content-end navbar  navbar-light ">
             <ul className="d-flex flex-row">
                 <li class="text-end d-flex flex-row">
                     <a id="emergacyLink" className="nav-link" href="#">חירום <span className="sr-only">(current)</span></a>
