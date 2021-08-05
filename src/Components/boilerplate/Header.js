d18b7f51877ce4ee25142f64a3b6f9670b8aa68c
avishsa <avishag1991@gmail.com>
2021-08-05 09:59:22 +0300

    header width

--- src/Components/boilerplate/Header.js
+++ src/Components/boilerplate/Header.js
@@ -4,7 +4,7 @@ import '../../css/boilerplate/Header.css';
 
 class Header extends Component {
     render() {
-        return (<header className="d-flex flex-row justify-content-between">
+        return (<header style={{ width: "428pt"}}  className="d-flex flex-row justify-content-between">
             <img src="./images/logo-app.png" style={{ width: "131pt", height: "131pt" }} />
             <NavApp/>
         </header>)
