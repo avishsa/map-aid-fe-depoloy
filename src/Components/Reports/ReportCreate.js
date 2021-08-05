a64a7248f4ea34f79436521281eeb42acf23ffae
avishsa <avishag1991@gmail.com>
2021-08-05 23:41:52 +0300

    date

--- src/Components/Reports/ReportCreate.js
+++ src/Components/Reports/ReportCreate.js
@@ -227,7 +227,7 @@ export default function ReportCreate(props) {
                     label={formFields.reportDateTime.label}
                     id={formFields.reportDateTime.name}
                     classNameLabel='required-astrix '
-                    
+                    classNameInput="text-end"
                 />
                 <MapGroup location={location.location_text} />
                 <HomelessDetails />
