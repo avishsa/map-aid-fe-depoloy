e08662e35a76776bcd54e82b338d457baa659cd2
Avishag Saban <avishag1991@gmail.com>
2021-08-10 22:56:42 +0300

    nav bar right pad marg

--- src/Components/boilerplate/NavApp.js
+++ src/Components/boilerplate/NavApp.js
@@ -2,7 +2,7 @@ import React, { Component }  from 'react';
 class NavApp extends Component {
     render() {
         return (<nav className=" d-flex flex-row   navbar  navbar-light ">
-            <ul className="d-flex flex-row">
+            <ul className="d-flex flex-row p-0 m-0">
                 <li className=" d-flex flex-row">
                     <a id="emergacyLink" className="nav-link" href="#">חירום <span className="sr-only">(current)</span></a>
                 </li>
