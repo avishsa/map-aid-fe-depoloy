7873f8ac267a4c7d8aa6c664355f181437a90cff
Avishag Saban <40865482+avishsa@users.noreply.github.com>
2021-09-09 22:28:24 +0300

    Update ReportWazeBtn.js

--- src/Components/Reports/ReportIndex/reportItem/ReportWazeBtn.js
+++ src/Components/Reports/ReportIndex/reportItem/ReportWazeBtn.js
@@ -2,7 +2,7 @@ import React from 'react';
 import BtnWaze from "../../../../images/waze.png"
 export default function ReportWazeBtn() {
     return (<div style={{ cursor: 'pointer' }} >
-        <a className="d-flex flex-row align-items-center col-10" href={`https://waze.com/ul?ll=${45.6906304},${-120.810983}&navigate=yes`}>
+        <button className="btn d-flex flex-row align-items-center col-10" href={`https://waze.com/ul?ll=${45.6906304},${-120.810983}&navigate=yes`}>
             <label style={{ cursor: 'pointer' }} htmlFor="BtnWaze" className="col-6"> נווט עם</label>
             <img
                 alt="go2waze"
@@ -11,6 +11,6 @@ export default function ReportWazeBtn() {
                 src={BtnWaze}
                 style={{ height: "10pt", width: "35pt" }}
             />
-        </a>
+        </button>
     </div>)
 }
