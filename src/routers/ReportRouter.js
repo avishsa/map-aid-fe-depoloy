1ecdb4d5dcf93946909a4d6e94a53bb96c1f9d47
Avishag Saban <40865482+avishsa@users.noreply.github.com>
2021-09-20 11:45:29 +0300

    auth

--- src/routers/ReportRouter.js
+++ src/routers/ReportRouter.js
@@ -1,37 +1,17 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
-import { Redirect } from 'react-router';
 import ReportCreate from '../Components/Reports/ReportCreate';
-import ReportIndex from '../Components/Reports/ReportIndex';
 import ReportSuccess from '../Components/Reports/ReportSuccess';
 import ReportFailure from '../Components/Reports/ReportFailure';
 import ReportMap from "../Components/Reports/ReportMap";
-import { authToken } from '../api/users';
-import { isLogged,loggedUser } from '../localStorage';
+import ProtectedRoute from './Authentication/ProtectedRoute';
 
 
 function ReportRouter() {
-  const isAuthenticated = localStorage.getItem(isLogged);
-  const loggedUserValue = localStorage.getItem(loggedUser);
+  
   return (<Switch>
     <Route exact path="/report/create" component={ReportCreate} />
-    <Route exact path="/report/Index" render={props => {
-     
-      console.log(localStorage.getItem('isLogged'));
-      if(isAuthenticated ==="true" && loggedUserValue ){ 
-        console.log(isAuthenticated)
-         return <ReportIndex {...props} />
-      } else{
-        console.log(isAuthenticated);
-        return <Redirect
-          to={{
-            pathname: "/user/login",
-
-          }}
-        />}
-    }
-    }
-    />
+    <ProtectedRoute path="/report/Index"/>
     <Route exact path="/report/success" component={ReportSuccess} />
     <Route exact path="/report/failure" component={ReportFailure} />
     <Route exact path="/report/map" component={ReportMap} />
