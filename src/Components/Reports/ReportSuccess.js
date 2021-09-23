91d4b0286d48cc8e6cf5d9b64bcd54fde5c7beb3
Avishag Saban <40865482+avishsa@users.noreply.github.com>
2021-09-23 11:08:16 +0300

    style Success report

--- src/Components/Reports/ReportSuccess.js
+++ src/Components/Reports/ReportSuccess.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import ListGap from "./ReportSuccess/ListGap";
 import "../../css/report/reportSuccess.css";
 const ReportSuccess = props => {
   const history = useHistory();
-  const preventDefault = e=>e.preventDefault();
-  return (<div>
+  const preventDefault = e => e.preventDefault();
+  return (<div className="d-flex flex-column">
     <h2>תודה שהודעת לנו!</h2>
     <h4> מה קורה עכשיו?</h4>
-    <ul className="d-flex flex-column p-0 m-0">
-      <li className=" d-flex flex-row ">
+    <ul className="d-flex flex-column  report-road-map-list">
+      <li className=" d-flex flex-column ">
         <div className="t">
-          <input className="form-check-input custom-control-input" type="checkbox" value="" id="flexCheckDefault" checked onClick={preventDefault}/>
+          <input className="form-check-input custom-control-input" type="checkbox" value="" id="flexCheckDefault" checked onClick={preventDefault} />
           <label className="form-check-label" for="flexCheckDefault" onClick={preventDefault}>
             הדיווח התקבל
           </label>
+        </div>
+        <ListGap />
+      </li>
 
-        </div></li>
-      <li className=" d-flex flex-row">
+      <li className=" d-flex flex-column">
         <div >
           <input className="form-check-input custom-control-input" type="checkbox" value="" id="flexCheckDefault" onClick={preventDefault} />
           <label className="form-check-label" for="flexCheckDefault" onClick={preventDefault}>
@@ -24,19 +27,21 @@ const ReportSuccess = props => {
           </label>
 
         </div>
+        <ListGap />
       </li>
-      <li className=" d-flex flex-row">
+      <li className=" d-flex flex-column">
         <div >
-          <input className="form-check-input custom-control-input" type="checkbox" value="" id="flexCheckDefault" onClick={preventDefault}/>
+          <input className="form-check-input custom-control-input" type="checkbox" value="" id="flexCheckDefault" onClick={preventDefault} />
           <label className="form-check-label" for="flexCheckDefault" onClick={preventDefault}>
             סייר יגיע עם הציוד הנדרש
           </label>
 
         </div>
+        <ListGap />
       </li>
-      <li className=" d-flex flex-row">
+      <li className=" d-flex flex-column">
         <div >
-          <input className="form-check-input custom-control-input" type="checkbox" value="" id="flexCheckDefault" onClick={preventDefault}/>
+          <input className="form-check-input custom-control-input" type="checkbox" value="" id="flexCheckDefault" onClick={preventDefault} />
           <label className="form-check-label" for="flexCheckDefault" onClick={preventDefault}>
             במידה וסימנת ישלח לך מסרון בסיום הטיפול
           </label>
@@ -44,11 +49,13 @@ const ReportSuccess = props => {
         </div>
       </li>
     </ul>
-    <input
-      value="תודה, נתראה בפעם הבאה"
-      type="button"
-      className=" redirectBtn btn text-center rounded-pill"
-      onClick={() => { history.push("/report/map"); }} />
+    <div className="d-flex flex-row justify-content-center">
+      <input
+        value="תודה, נתראה בפעם הבאה"
+        type="button"
+        className=" redirectBtn btn text-center rounded-pill"
+        onClick={() => { history.push("/report/map"); }} />
+    </div>
   </div>);
 };
 export default ReportSuccess;
---
+++ src/Components/Reports/ReportSuccess/ListGap.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function ListGap(){
+    return (<div className="mx-2" style={{height: "20pt",borderRight: "1pt solid rgb(112,112,112)"}}> </div>);
+}
