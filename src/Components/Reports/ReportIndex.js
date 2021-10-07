1b2a36514041542ee91835d0591e2d18bf62e2d9
Avishag Saban <40865482+avishsa@users.noreply.github.com>
2021-10-07 12:45:00 +0300

    style header
    style app header - sticky top
    style report index header - fixed

--- src/Components/Reports/ReportIndex.js
+++ src/Components/Reports/ReportIndex.js
@@ -1,12 +1,12 @@
-import React, {useEffect} from 'react';
-import { useSelector ,useDispatch} from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 
 import NavReports from '../Reports/ReportIndex/NavReports'
 import FormReports from './ReportIndex/FormReports';
 import ReportItem from './ReportIndex/ReportItem';
 
-import {reportActions} from "../../actions/reportActions";
+import { reportActions } from "../../actions/reportActions";
 import "../../css/report/reportIndex.css"
 
 
@@ -14,16 +14,16 @@ import "../../css/report/reportIndex.css"
 
 export default function ReportIndex() {
 
-    const user = useSelector(state => { return state.authentication.user});
-    const reports =  useSelector(state => { return state.reports});
+    const user = useSelector(state => { return state.authentication.user });
+    const reports = useSelector(state => { return state.reports });
     const filterParam = useSelector(state => { ; return state.reports.property });
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(reportActions.getAll(user.id));
-    }, [dispatch,user]);
-    
-      
+    }, [dispatch, user]);
+
+
     const getBorderColor = (isHandled) => {
 
         switch (isHandled) {
@@ -32,22 +32,25 @@ export default function ReportIndex() {
             default: return 'rgb(136 ,137, 138)';
         }
     }
-    
-    
-    return (<div className="d-flex flex-column justify-content-center" style={{height:"410pt"}}>
-        {user.name && <h1 className="text-end">{`היי ${user.name}`}</h1>}
-        <NavReports  />
-        {filterParam!==undefined &&<FormReports   />}
-        {reports.items_filtered && (<ul dir="ltr" className="list-group overflow-auto" style={{ paddingInlineStart: '0 !important' }}>
+
+
+    return (<div className="d-flex flex-column justify-content-center" /*style={{height:"410pt"}}*/>
+
+        <div id="headerPage" style={{"position": "fixed","top":"70pt","width": "100%","zIndex": "1040","backgroundColor":"rgb(233, 236, 241)"}}>
+            {user.name && <h1 className="text-end">{`היי ${user.name}`}</h1>}
+            <NavReports />
+            {filterParam !== undefined && <FormReports />}
+        </div>
+        {reports.items_filtered && (<ul dir="ltr" className="list-group overflow-auto" style={{marginTop:"100pt", paddingInlineStart: '0 !important' }}>
             {reports.items_filtered.map((report, index) => (
-                <li   dir="rtl" className="list-group-item my-2"
+                <li dir="rtl" className="list-group-item my-2"
                     style={{ 'borderTop': `solid ${getBorderColor(report.isHandled)} 3pt` }}
                     key={index}>
-                    <ReportItem LOGGEDUSER={user.id} report={report}/>
+                    <ReportItem LOGGEDUSER={user.id} report={report} />
                 </li>
             ))}
         </ul>)}
-       
+
     </div>);
 
 }
--- src/Components/boilerplate/Header.js
+++ src/Components/boilerplate/Header.js
@@ -4,7 +4,7 @@ import '../../css/boilerplate/Header.css';
 import AppLogo from "../../images/logo-app.png";
 export default function Header(/*{token}*/) {
     
-        return (<header className=" d-flex  flex-row p-0 m-0 mb-3" >
+        return (<header className="sticky-top d-flex  flex-row p-0 m-0 pb-3" style={{"backgroundColor": "rgb(233, 236, 241)"}} >
             <div style={{position:"relative"}} className="d-flex flex-column-reverse justify-content-start col-12 ">
                 <div className="d-flex flex-row justify-content-end" style={{position:"relative",bottom:"13pt"}} >
                     <img src={AppLogo} alt="logo elem" /> 
